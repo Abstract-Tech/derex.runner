9bf93d0dc182c2196d08ea8ae74135b7c940f8c4
Silvio Tomatis <silviot@gmail.com>
2019-06-26 00:28:50 +0200

    Cleanup

--- derex/runner/utils.py
+++ derex/runner/utils.py
@@ -49,14 +49,12 @@ truthy = frozenset(("t", "true", "y", "yes", "on", "1"))
 def asbool(s):
     """ Return the boolean value ``True`` if the case-lowered value of string
     input ``s`` is a :term:`truthy string`. If ``s`` is already one of the
-    boolean values ``True`` or ``False``, return it."""
+    boolean values ``True`` or ``False``, return it.
+    Lifted from pyramid.settings.
+    """
     if s is None:
         return False
     if isinstance(s, bool):
         return s
     s = str(s).strip()
     return s.lower() in truthy
-
-
-def start_service():
-    run_compose(["ps"])
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,13 @@
 """Tests for `derex.runner.utils`."""
 
 
-def test_start_service():
-    from derex.runner.utils import start_service
+def test_asbool():
+    """It's lifted from pyramid.settings, but testing it here won't harm.
+    """
+    from derex.runner.utils import asbool
 
-    start_service()
+    assert asbool("True") is True
+    assert asbool("false") is False
+    assert asbool(0) is False
+    assert asbool("0") is False
+    assert asbool(None) is False
