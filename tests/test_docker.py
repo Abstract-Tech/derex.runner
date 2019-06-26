5e2c99d59804ba6e92fce1f8f94b38683af45f3c
Silvio Tomatis <silviot@gmail.com>
2019-06-26 23:00:32 +0200

    Test for volumes present

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from types import SimpleNamespace
 import docker
 
 
@@ -12,3 +13,21 @@ def test_ensure_network_present(mocker):
     client.networks.get.side_effect = docker.errors.NotFound("derex network not found")
     ensure_network_present()
     client.networks.create.assert_called_once_with("derex")
+
+
+def test_ensure_volumes_present(mocker):
+    from derex.runner.docker import ensure_volumes_present
+    from derex.runner.docker import VOLUMES
+
+    client = mocker.patch("derex.runner.docker.client")
+
+    client.volumes.list.return_value = []
+    ensure_volumes_present()
+    assert client.volumes.create.call_count > 3
+    client.volumes.create.assert_any_call("derex_mysql")
+    client.volumes.create.assert_any_call("derex_mongodb")
+
+    client.volumes.create.reset_mock()
+    client.volumes.list.return_value = [SimpleNamespace(name=name) for name in VOLUMES]
+    ensure_volumes_present()
+    client.volumes.create.assert_not_called()
