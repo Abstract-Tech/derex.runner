f485a550adc3c0ce988d16984c8d235f2cdd80e7
Silvio Tomatis <silviot@gmail.com>
2019-06-26 17:30:19 +0200

    Avoid CVE-2017-18342 updating pyyaml to 4.2b4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["Click>=6.0", "docker-compose"]
+requirements = ["Click>=6.0", "docker-compose", "pyyaml>=4.2b4"]
 
 setup_requirements = ["pytest-runner"]
 
