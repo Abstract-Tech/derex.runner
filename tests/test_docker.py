641b2f95f2172488095bd9b6e379119a6cdcba3c
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2019-08-05 14:20:52 +0200

    Removed "ensure_network_presence" function and test

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -3,18 +3,6 @@ from types import SimpleNamespace
 import docker
 
 
-def test_ensure_network_present(mocker):
-    from derex.runner.docker import ensure_network_present
-
-    client = mocker.patch("derex.runner.docker.client")
-    ensure_network_present()
-    client.networks.get.assert_called_once()
-
-    client.networks.get.side_effect = docker.errors.NotFound("derex network not found")
-    ensure_network_present()
-    client.networks.create.assert_called_once_with("derex")
-
-
 def test_ensure_volumes_present(mocker):
     from derex.runner.docker import ensure_volumes_present
     from derex.runner.docker import VOLUMES
