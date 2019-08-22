6cc5148b0e947ea2434a5e9fad1792cbc46d75c8
Silvio Tomatis <silviot@gmail.com>
2019-08-22 15:41:10 +0200

    Find project base dir and config

--- derex/runner/utils.py
+++ derex/runner/utils.py
@@ -1,4 +1,37 @@
+from pathlib import Path
+from typing import Union
+import os
 import pkg_resources
+import yaml
+
+
+CONF_FILENAME = ".derex.config.yaml"
+
+
+def compose_path(name):
+    return pkg_resources.resource_filename(__name__, f"compose_files/{name}")
+
+
+def project_config():
+    """Return the parsed configuration of this project.
+    """
+    dir = project_dir(os.getcwd())
+    filename = dir / CONF_FILENAME
+    return yaml.load(filename.open())
+
+
+def project_dir(path: Union[Path, str]):
+    """Find the project directory walking up the filesystem starting on the
+    given path until a configuration file is found.
+    """
+    current = Path(path)
+    while current != current.parent:
+        if (current / CONF_FILENAME).is_file():
+            return current
+        current = current.parent
+    raise ValueError(
+        f"No directory found with a {CONF_FILENAME} file in it, starting from {path}"
+    )
 
 
 truthy = frozenset(("t", "true", "y", "yes", "on", "1"))
@@ -16,7 +49,3 @@ def asbool(s):
         return s
     s = str(s).strip()
     return s.lower() in truthy
-
-
-def compose_path(name):
-    return pkg_resources.resource_filename(__name__, f"compose_files/{name}")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,28 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from pathlib import Path
+import contextlib
+import os
 
-"""Tests for `derex.runner.utils`."""
+
+MINIMAL_PROJ = Path(__file__).parent / "fixtures" / "minimal"
+
+
+def test_project_config():
+    """
+    """
+    from derex.runner.utils import project_config
+
+    with working_directory(MINIMAL_PROJ):
+        res = project_config()
+    assert res["project_name"] == "minimal"
+
+
+def test_project_dir():
+    """
+    """
+    from derex.runner.utils import project_dir
+
+    assert project_dir(MINIMAL_PROJ) == project_dir(MINIMAL_PROJ / "themes")
 
 
 def test_asbool():
@@ -15,3 +36,14 @@ def test_asbool():
     assert asbool(0) is False
     assert asbool("0") is False
     assert asbool(None) is False
+
+
+@contextlib.contextmanager
+def working_directory(path: Path):
+    """Changes working directory and returns to previous on exit."""
+    prev_cwd = Path.cwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
