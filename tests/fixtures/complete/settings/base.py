8d4b8d1120b61fd82cf87722ecd3fab96109d394
Silvio Tomatis <silviot@gmail.com>
2019-09-04 17:43:45 +0200

    Fix site name

--- tests/fixtures/complete/settings/base.py
+++ tests/fixtures/complete/settings/base.py
@@ -1,7 +1,15 @@
 import os
+import sys
 
 
 SERVICE_VARIANT = os.environ["SERVICE_VARIANT"]
 assert SERVICE_VARIANT in ("lms", "cms")
-
 exec("from {}.envs.derex.base import *".format(SERVICE_VARIANT), globals(), locals())
+
+if SERVICE_VARIANT == "lms":
+    SITE_NAME = "localhost:4700"
+else:
+    SITE_NAME = "localhost:4800"
+
+if "runserver" in sys.argv:
+    SITE_NAME = SITE_NAME[:-1] + "1"
