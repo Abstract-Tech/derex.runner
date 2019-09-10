d2dac7b23849cb9113626dd83797fbced137925f
Silvio Tomatis <silviot@gmail.com>
2019-09-11 00:23:05 +0200

    Improved base.py

--- tests/fixtures/complete/settings/base.py
+++ tests/fixtures/complete/settings/base.py
@@ -1,3 +1,5 @@
+from openedx.core.djangoapps.plugins import constants as plugin_constants
+from openedx.core.djangoapps.plugins import plugin_settings
 from openedx.core.lib.derived import derive_settings
 from path import Path
 
@@ -7,6 +9,7 @@ import sys
 
 SERVICE_VARIANT = os.environ["SERVICE_VARIANT"]
 assert SERVICE_VARIANT in ("lms", "cms")
+PROJECT_TYPE = getattr(plugin_constants.ProjectType, SERVICE_VARIANT.upper())
 exec("from {}.envs.derex.base import *".format(SERVICE_VARIANT), globals(), locals())
 
 if SERVICE_VARIANT == "lms":
@@ -17,11 +20,17 @@ else:
 if "runserver" in sys.argv:
     SITE_NAME = SITE_NAME[:-1] + "1"
 
+STATIC_ROOT_BASE = "/openedx/staticfiles"
+
 MEDIA_ROOT = "/openedx/media"
 VIDEO_TRANSCRIPTS_SETTINGS["location"] = MEDIA_ROOT  # type: ignore  # noqa
 VIDEO_IMAGE_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT  # type: ignore  # noqa
 PROFILE_IMAGE_BACKEND["options"]["location"] = MEDIA_ROOT  # type: ignore  # noqa
 COMPREHENSIVE_THEME_DIRS.append(Path("/openedx/themes"))  # type: ignore  # noqa
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+plugin_settings.add_plugins(
+    __name__, PROJECT_TYPE, plugin_constants.SettingsType.PRODUCTION
+)
 
 derive_settings(__name__)
