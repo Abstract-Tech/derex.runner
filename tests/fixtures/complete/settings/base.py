938913f12590586ec2d8157ee7f4bbb84f4e9c37
Silvio Tomatis <silviot@gmail.com>
2019-09-17 21:58:15 +0200

    Use SITE_ID coupled with database fixtures to set theme regardless of domain

--- tests/fixtures/complete/settings/base.py
+++ tests/fixtures/complete/settings/base.py
@@ -1,6 +1,3 @@
-from openedx.core.djangoapps.plugins import constants as plugin_constants
-from openedx.core.djangoapps.plugins import plugin_settings
-from openedx.core.lib.derived import derive_settings
 from path import Path
 
 import os
@@ -9,15 +6,8 @@ import sys
 
 SERVICE_VARIANT = os.environ["SERVICE_VARIANT"]
 assert SERVICE_VARIANT in ("lms", "cms")
-PROJECT_TYPE = getattr(plugin_constants.ProjectType, SERVICE_VARIANT.upper())
 exec("from {}.envs.derex.base import *".format(SERVICE_VARIANT), globals(), locals())
 
-if SERVICE_VARIANT == "lms":
-    SITE_NAME = "localhost:4700"
-else:
-    SITE_NAME = "localhost:4800"
 
-if "runserver" in sys.argv:
-    SITE_NAME = SITE_NAME[:-1] + "1"
-
-derive_settings(__name__)
+# Id of the site fixture to use, instead of looking up the hostname
+SITE_ID = 1
