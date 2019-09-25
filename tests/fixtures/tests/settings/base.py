3a8fd0dfd04679a74489f160831eae5cf3e52c0b
Silvio Tomatis <silviot@gmail.com>
2019-09-25 14:10:37 +0200

    Add fixture to run e2e tests

---
+++ tests/fixtures/tests/settings/base.py
@@ -0,0 +1,9 @@
+import os
+
+
+SERVICE_VARIANT = os.environ["SERVICE_VARIANT"]
+assert SERVICE_VARIANT in ("lms", "cms")
+exec("from {}.envs.derex.base import *".format(SERVICE_VARIANT), globals(), locals())
+
+SITE_NAME = "{}.edx.localhost".format(SERVICE_VARIANT)
+HTTPS = "on"
