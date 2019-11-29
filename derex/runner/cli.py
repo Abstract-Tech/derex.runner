65ae5bb6a720aea591ccd7900541f4fb2b29e982
Silvio Tomatis <silviot@gmail.com>
2019-11-29 16:27:47 +0100

    WIP Make ddc-project a pure wrapper instead of click command

--- derex/runner/cli.py
+++ derex/runner/cli.py
@@ -4,10 +4,12 @@
 from derex.runner.docker import check_services
 from derex.runner.docker import execute_mysql_query
 from derex.runner.docker import load_dump
+from derex.runner.docker import pull_images
 from derex.runner.docker import wait_for_mysql
 
 import click
 import logging
+import os
 
 
 logger = logging.getLogger(__name__)
@@ -15,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 @click.group()
 def derex():
-    """Derex directs edX.
+    """Derex directs edX: commands to manage an Open edX installation
     """
 
 
@@ -32,3 +34,74 @@ def reset_mailslurper():
     click.echo("Priming mailslurper database")
     load_dump("fixtures/mailslurper.sql")
     return 0
+
+
+@derex.command()
+def compile_theme():
+    """Compile theme sass files"""
+    if project.themes_dir is None:
+        click.echo("No theme directory present in this project")
+        return
+    themes = " ".join(el.name for el in project.themes_dir.iterdir())
+    uid = os.getuid()
+    args = [
+        "run",
+        "--rm",
+        "lms",
+        "sh",
+        "-c",
+        f"""set -ex
+            export PATH=/openedx/edx-platform/node_modules/.bin:$PATH  # FIXME: this should not be necessary
+            paver compile_sass --theme-dirs /openedx/themes --themes {themes}
+            chown {uid}:{uid} /openedx/themes/* -R""",
+    ]
+    run_compose(args, project=project, dry_run=dry_run)
+    return
+
+
+@derex.command()
+def reset_mysql():
+    """Reset mysql database for the project"""
+    if not check_services(["mysql"]):
+        click.echo("Mysql service not found.\nMaybe you forgot to run\nddc up -d")
+        return
+    resetdb(project, dry_run=dry_run)
+    return 0
+
+
+@derex.command()
+def reset_rabbitmq():
+    """Create rabbitmq vhost"""
+    vhost = f"{project.name}_edxqueue"
+    args = [
+        "exec",
+        "rabbitmq",
+        "sh",
+        "-c",
+        f"""rabbitmqctl add_vhost {vhost}
+        rabbitmqctl set_permissions -p {vhost} guest ".*" ".*" ".*"
+        """,
+    ]
+    run_compose(args, dry_run=dry_run)
+    return 0
+
+
+@derex.command()
+def build_requirements():
+    """Build the image that contains python requirements"""
+
+
+@derex.command()
+def build_themes():
+    """Build the image that includes compiled themes"""
+
+
+@derex.command()
+def build_final():
+    """Build the final image for this project"""
+
+
+@derex.command()
+def build_final_refresh():
+    """Also pull base docker image before starting building"""
+    pull_images([project.base_image, project.final_base_image])
--- derex/runner/ddc.py
+++ derex/runner/ddc.py
@@ -3,22 +3,33 @@
 These wrappers invoke `docker-compose` functions to get their job done.
 They put a `docker.compose.yml` file in place based on user configuration.
 """
-from derex.runner.build import build_requirements_image
-from derex.runner.build import build_themes_image
 from derex.runner.compose_utils import reset_mysql
 from derex.runner.compose_utils import run_compose
 from derex.runner.docker import check_services
 from derex.runner.docker import execute_mysql_query
 from derex.runner.docker import is_docker_working
-from derex.runner.docker import pull_images
 from derex.runner.docker import wait_for_mysql
 from derex.runner.project import Project
+from typing import List
 from typing import Tuple
 
 import click
 import logging
-import os
 import sys
+
+
+def ddc_parse_args(args: List[str]) -> Tuple[List[str], bool]:
+    """Given a list of args, extract the ones to be passed to docker-compose
+    (basically just omit the first one) and return the adjusted list.
+
+    Also checks if the `--dry-run` flag is present, removes it from the
+    list of args if it is and returns a 2-tuple like `(args, dry_run)`
+    """
+    dry_run = False
+    if "--dry-run" in args:
+        dry_run = True
+        args = [el for el in args if el != "--dry-run"]
+    return args[1:], dry_run
 
 
 def ddc_services():
@@ -34,37 +45,16 @@ def ddc_services():
     """
     check_docker()
     setup_logging()
-    args = sys.argv[1:]
-    dry_run = False
-    if "--dry-run" in args:
-        dry_run = True
-        args = [el for el in args if el != "--dry-run"]
+    args, dry_run = ddc_parse_args(sys.argv)
     run_compose(args, dry_run=dry_run)
     return 0
 
 
-@click.command(context_settings=dict(ignore_unknown_options=True))
-@click.argument("compose_args", nargs=-1)
-@click.option(
-    "--dry-run",
-    default=False,
-    is_flag=True,
-    help="Don't actually do anything, just print what would have been run",
-)
-def ddc_project(compose_args: Tuple[str, ...], dry_run: bool):
+def ddc_project():
     """Proxy for docker-compose: writes a docker-compose.yml file with the
     configuration of this project, and then run `docker-compose` on it.
 
-    You probably want do run `ddc-local up -d` and `ddc-local logs -f`.
-
-    Besides this, also accept these commands:\n
-        * compile-theme (compile theme sass files)\n
-        * reset-mysql (reset mysql database for the project)\n
-        * reset-rabbitmq (create rabbitmq vhost)\n
-        * build-requirements (build the image that contains python requirements)\n
-        * build-themes (build the image that includes compiled themes)\n
-        * build-final (build the final image for this project)\n
-        * build-final-refresh (also pull base docker image before starting)\n
+    You probably want do run `ddc-project up -d` and `ddc-project logs -f`.
     """
     check_docker()
     setup_logging()
@@ -73,82 +63,15 @@ def ddc_project(compose_args: Tuple[str, ...], dry_run: bool):
     except ValueError:
         click.echo("You need to run this command in a derex project")
         sys.exit(1)
-    BUILD_COMMANDS = [
-        "build-requirements",
-        "build-themes",
-        "build-final",
-        "build-final-refresh",
-    ]
-    COMMANDS = BUILD_COMMANDS + ["reset-mysql", "reset-rabbitmq", "compile-theme"]
-    command = ""
-    if len(compose_args) == 1 and compose_args[0] in COMMANDS:
-        command = compose_args[0]
-    else:
-        if not check_services(["mysql", "mongodb", "rabbitmq"]) and any(
-            param in compose_args for param in ["up", "start"]
-        ):
-            click.echo(
-                "Mysql/mongo/rabbitmq services not found.\nMaybe you forgot to run\nddc up -d"
-            )
-            return
-        run_compose(list(compose_args), project=project, dry_run=dry_run)
-
-    if command == "build-final-refresh":
-        pull_images([project.base_image, project.final_base_image])
-    if command in BUILD_COMMANDS:
+    compose_args, dry_run = ddc_parse_args(sys.argv)
+    # If trying to start up containers, first check that needed services are running
+    is_start_cmd = any(param in compose_args for param in ["up", "start"])
+    if is_start_cmd and not check_services(["mysql", "mongodb", "rabbitmq"]):
         click.echo(
-            f'Building docker image {project.requirements_image_tag} with "{project.name}" project requirements'
+            "Mysql/mongo/rabbitmq services not found.\nMaybe you forgot to run\nddc-services up -d"
         )
-        build_requirements_image(project)
-    if command in ["build-themes", "build-final", "build-final-refresh"]:
-        click.echo(
-            f'Building docker image {project.themes_image_tag} with "{project.name}" themes'
-        )
-        build_themes_image(project)
-    if command.startswith("build-"):
-        click.echo(f"Built image {project.themes_image_tag}")
         return
-
-    if command == "compile-theme":
-        if project.themes_dir is None:
-            click.echo("No theme directory present in this project")
-            return
-        themes = " ".join(el.name for el in project.themes_dir.iterdir())
-        uid = os.getuid()
-        args = [
-            "run",
-            "--rm",
-            "lms",
-            "sh",
-            "-c",
-            f"""set -ex
-                export PATH=/openedx/edx-platform/node_modules/.bin:$PATH  # FIXME: this should not be necessary
-                paver compile_sass --theme-dirs /openedx/themes --themes {themes}
-                chown {uid}:{uid} /openedx/themes/* -R""",
-        ]
-        run_compose(args, project=project, dry_run=dry_run)
-        return
-
-    if command == "reset-mysql":
-        if not check_services(["mysql"]):
-            click.echo("Mysql service not found.\nMaybe you forgot to run\nddc up -d")
-            return
-        resetdb(project, dry_run=dry_run)
-        return
-
-    if command == "reset-rabbitmq":
-        vhost = f"{project.name}_edxqueue"
-        args = [
-            "exec",
-            "rabbitmq",
-            "sh",
-            "-c",
-            f"""rabbitmqctl add_vhost {vhost}
-            rabbitmqctl set_permissions -p {vhost} guest ".*" ".*" ".*"
-            """,
-        ]
-        run_compose(args, dry_run=dry_run)
-        return
+    run_compose(list(compose_args), project=project, dry_run=dry_run)
 
 
 def setup_logging():
