c175b076b0701844be1c20e98b5bf89770f56b1b
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2019-12-02 16:10:22 +0100

    Updated all occurrencies of obsolete ddc and ddc-local

--- tests/fixtures/notes/setup.sh
+++ tests/fixtures/notes/setup.sh
@@ -2,5 +2,5 @@
 
 # This script is temporary, while we figure out how to manage components exactly
 
-ddc exec mysql mysql -psecret -e "CREATE DATABASE IF NOT EXISTS derex_notes"
-ddc-local run --rm notes ./manage.py migrate
+ddc-services exec mysql mysql -psecret -e "CREATE DATABASE IF NOT EXISTS derex_notes"
+ddc-project run --rm notes ./manage.py migrate
