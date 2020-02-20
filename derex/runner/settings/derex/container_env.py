a8a17deb7e691ca9d4f449f842649ca50df6a908
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-02-20 11:34:33 +0100

    Load settings for wildcard service too

--- derex/runner/settings/derex/container_env.py
+++ derex/runner/settings/derex/container_env.py
@@ -3,9 +3,13 @@ import os
 
 SERVICE_VARIANT = os.environ["SERVICE_VARIANT"]
 
+wildcard_prefix = "DEREX_" + "*" + "_"
 prefix = "DEREX_" + SERVICE_VARIANT.upper() + "_"
 
 for key, value in os.environ.items():
     if key.startswith(prefix):
         varname = key[len(prefix) :]  # noqa  This is the way black likes it
+        locals()[varname] = value
+    elif key.startswith(wildcard_prefix):
+        varname = key[len(wildcard_prefix) :]  # noqa  This is the way black likes it
         locals()[varname] = value
