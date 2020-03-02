1cbbecab359c5c084cb6e1b0c4c1f2766617839d
Silvio Tomatis <silviot@gmail.com>
2020-03-02 15:46:59 +0100

    Customize logo display

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath(".."))
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ["html_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -84,10 +84,14 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_logo = "assets/logo.svg"
 html_favicon = "assets/favicon.ico"
+
+html_css_files = [
+    "css/custom.css",
+]
 
 
 # -- Options for HTMLHelp output ---------------------------------------
