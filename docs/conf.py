1181ff034791a4119fd9e0d95b400e99f0e47958
Silvio Tomatis <silviot@gmail.com>
2020-03-02 15:04:46 +0100

    Add derex logo

--- docs/conf.py
+++ docs/conf.py
@@ -86,6 +86,8 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 
+html_logo = "logo.svg"
+
 
 # -- Options for HTMLHelp output ---------------------------------------
 
