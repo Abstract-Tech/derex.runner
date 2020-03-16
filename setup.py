07cc63fd35c5fbf6499570a08eb230101fc69e9a
Silvio Tomatis <silviot@gmail.com>
2020-03-16 16:22:32 +0100

    Remove python versions <3.6 and add 3.8

--- setup.py
+++ setup.py
@@ -36,13 +36,9 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     entry_points={
         "console_scripts": [
