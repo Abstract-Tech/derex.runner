5f1ac457bd5532164aa1b77cbbcdde79a0b8b7e9
Silvio Tomatis <silviot@gmail.com>
2020-03-19 12:23:37 +0100

    Improve coverage

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -12,4 +12,10 @@ def test_registry_basic():
     )
 
     assert registry["last"] == "I should be last"
+    assert registry[-1] == "I should be last"
     assert registry["first"] == "I should be first"
+    assert registry[0] == "I should be first"
+
+    # Now move the last to the beginning, just to prove we can
+    registry.add("last", "I should be last", "_begin")
+    assert registry[0] == "I should be last"
