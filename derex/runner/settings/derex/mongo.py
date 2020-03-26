c0d74f5f56de950aa16145b46447f8d9d2c85c59
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-03-26 09:47:16 +0100

    Define a unique coherent default name for mysql and mongodb database

--- derex/runner/settings/derex/mongo.py
+++ derex/runner/settings/derex/mongo.py
@@ -2,21 +2,13 @@ from xmodule.modulestore.modulestore_settings import update_module_store_setting
 
 
 MONGODB_HOST = "mongodb"
-MONGODB_DB = os.environ.get("MONGO_DB", "mongoedx")
+MONGODB_DB_NAME = os.environ["MONGODB_DB_NAME"]
 CONTENTSTORE = {
     "ENGINE": "xmodule.contentstore.mongo.MongoContentStore",
-    "DOC_STORE_CONFIG": {"host": MONGODB_HOST, "db": MONGODB_DB},
+    "DOC_STORE_CONFIG": {"host": MONGODB_HOST, "db": MONGODB_DB_NAME},
 }
-DOC_STORE_CONFIG = {"host": MONGODB_HOST, "db": MONGODB_DB}
+DOC_STORE_CONFIG = {"host": MONGODB_HOST, "db": MONGODB_DB_NAME}
 update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
 
 # This is needed for the Sysadmin dashboard "Git Logs" tab
 MONGODB_LOG = {"host": DOC_STORE_CONFIG["host"], "db": "xlog"}
-
-if DEREX_PROJECT:
-    # Mongodb
-    MONGODB_DB = "{}_mongoedx".format(DEREX_PROJECT)
-    DOC_STORE_CONFIG["db"] = MONGODB_DB
-    CONTENTSTORE["DOC_STORE_CONFIG"] = DOC_STORE_CONFIG
-    for store in MODULESTORE["default"]["OPTIONS"]["stores"]:
-        store["DOC_STORE_CONFIG"]["db"] = MONGODB_DB
