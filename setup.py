e14d49146900a43b821fab43db48c071f8393ec4
Silvio Tomatis <silviot@gmail.com>
2020-04-02 10:26:02 +0200

    Bump version: 0.0.2.dev1 → 0.0.2.dev2

--- setup.py
+++ setup.py
@@ -61,6 +61,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extras_requirements,
     url="https://github.com/Abstract-Tech/derex.runner",
-    version="0.0.2.dev1",
+    version="0.0.2.dev2",
     zip_safe=False,
 )
