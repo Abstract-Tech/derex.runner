c5014df4d28ac6b06d1df5a7507065221fc5745a
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-04-07 17:21:03 +0200

    Fix profile images and video transcripts upload location

--- derex/runner/settings/derex/media.py
+++ derex/runner/settings/derex/media.py
@@ -1,4 +1,9 @@
 MEDIA_ROOT = "/openedx/media"
-VIDEO_TRANSCRIPTS_SETTINGS["location"] = MEDIA_ROOT
+VIDEO_TRANSCRIPTS_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
 VIDEO_IMAGE_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
-PROFILE_IMAGE_BACKEND["options"]["location"] = MEDIA_ROOT
+PROFILE_IMAGE_BACKEND["options"].update(
+    {
+        "base_url": os.path.join(MEDIA_URL, "profile-images/"),
+        "location": os.path.join(MEDIA_ROOT, "profile-images/"),
+    }
+)
