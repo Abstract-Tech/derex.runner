8ef8bac42ef4e6986a65038004d74342b5bd3ada
Silvio Tomatis <silviot@gmail.com>
2020-04-07 17:32:20 +0200

    Remove the SettingsModified exception: warn the user and proceed to overwrite files (showing a diff)

--- derex/runner/cli.py
+++ derex/runner/cli.py
@@ -8,7 +8,6 @@ from derex.runner.project import DebugBaseImageProject
 from derex.runner.project import OpenEdXVersions
 from derex.runner.project import Project
 from derex.runner.project import ProjectRunMode
-from derex.runner.project import SettingsModified
 from derex.runner.utils import abspath_from_egg
 from distutils.spawn import find_executable
 from functools import wraps
@@ -50,14 +49,7 @@ def derex(ctx):
     # Optimize --help and bash completion by importing
     from derex.runner.project import Project
 
-    try:
-        ctx.obj = Project()
-    except SettingsModified as error:
-        print(
-            f"Derex settings file modified:\n{error.filename}\nDelete or rename the file, and we'll put back the stock version"
-        )
-    except ValueError:
-        pass
+    ctx.obj = Project()
 
 
 @derex.command()
@@ -330,7 +322,7 @@ def get_available_settings():
     """Return settings available on the current project"""
     try:
         project = Project()
-    except (ValueError, SettingsModified):
+    except ValueError:
         return None
     return project.get_available_settings().__members__
 
