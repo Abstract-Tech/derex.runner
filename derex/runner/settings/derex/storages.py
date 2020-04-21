a6d0a3d5d5910a7c531ebf1cc3459cc6a95edb7b
Silvio Tomatis <silviot@gmail.com>
2020-04-21 19:12:48 +0200

    Configure minio hostname for file upload

--- derex/runner/settings/derex/storages.py
+++ derex/runner/settings/derex/storages.py
@@ -3,19 +3,40 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 AWS_ACCESS_KEY_ID = "minio_derex"
 AWS_SECRET_ACCESS_KEY = "derex_default_secret"
 
+
 AWS_S3_CALLING_FORMAT = "boto.s3.connection.OrdinaryCallingFormat"
-AWS_S3_HOST = "minio"
+AWS_S3_HOST = "minio.localhost"
 AWS_S3_PORT = 80
 AWS_S3_USE_SSL = False
 AWS_QUERYSTRING_AUTH = False
 S3_USE_SIGV4 = True
+ORA2_FILEUPLOAD_BACKEND = "s3"
+
+# Hack lifted from tutor-minio
+# Configuring boto is required for ora2 because ora2 does not read
+# host/port/ssl settings from django. Hence this hack.
+# http://docs.pythonboto.org/en/latest/boto_config_tut.html
+import os
+
+
+os.environ["AWS_CREDENTIAL_FILE"] = "/tmp/boto.cfg"
+with open("/tmp/boto.cfg", "w") as f:
+    f.write(
+        """[Boto]
+is_secure = False
+[s3]
+host = {}
+calling_format = boto.s3.connection.OrdinaryCallingFormat""".format(
+            AWS_S3_HOST
+        )
+    )
 
 COURSE_IMPORT_EXPORT_STORAGE = DEFAULT_FILE_STORAGE
 USER_TASKS_ARTIFACT_STORAGE = DEFAULT_FILE_STORAGE
 
 
 # Bucket names: they need to be kept in sync with project.py
-AWS_STORAGE_BUCKET_NAME = DEREX_PROJECT
+FILE_UPLOAD_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME = DEREX_PROJECT
 
 FILE_UPLOAD_STORAGE_PREFIX = "submissions_attachments"
 
