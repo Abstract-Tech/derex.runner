06dfebeaefc64f1b89e3c9cd24f8ef923a77b22d
Silvio Tomatis <silviot@gmail.com>
2020-04-23 18:05:14 +0200

    New get_secret function to get service and project specific secret

--- derex/runner/secrets.py
+++ derex/runner/secrets.py
@@ -1,5 +1,7 @@
 """Tools to deal with secrets in derex.
 """
+from base64 import b64encode
+from hashlib import scrypt
 from pathlib import Path
 
 import logging
@@ -9,7 +11,11 @@ import os
 logger = logging.getLogger(__name__)
 
 
-def get_master_secret():
+DEREX_MAIN_SECRET_MAX_SIZE = 1024
+DEREX_MAIN_SECRET_PATH = "/etc/derex/main_secret"
+
+
+def get_master_secret() -> str:
     """Derex uses a master secret to derive all other secrets.
     This functions finds the master secret on the current machine,
     and if it can't find it it will return a default one.
@@ -17,12 +23,32 @@ def get_master_secret():
     The default location is `/etc/derex/main_secret`, but can be customized
     via the environment variable DEREX_MAIN_SECRET_PATH.
     """
-    filepath = Path(os.environ.get("DEREX_MAIN_SECRET_PATH", "/etc/derex/main_secret"))
+    filepath = Path(os.environ.get("DEREX_MAIN_SECRET_PATH", DEREX_MAIN_SECRET_PATH))
+    max_size = int(
+        os.environ.get("DEREX_MAIN_SECRET_MAX_SIZE", DEREX_MAIN_SECRET_MAX_SIZE)
+    )
 
     if os.access(filepath, os.R_OK):
-        return filepath.read_text().strip()
+        master_secret = filepath.read_text().strip()
+        assert (
+            len(master_secret) < max_size
+        ), f"Master secret in {filepath} is too large: {len(master_secret)}"
+        return master_secret
 
     if filepath.exists():
         logger.error(f"File filepath is not readable; using default master secret")
 
     return "Default secret"
+
+
+def get_secret(name: str) -> str:
+    """Derive a secret using the master secret and the provided name.
+    """
+    master_secret = get_master_secret()
+    binary_secret = scrypt(
+        master_secret.encode("utf-8"), salt=name.encode("utf-8"), n=2, r=8, p=1
+    )
+    # Pad the binary string so that its length is a multiple of 3
+    # This will make sure its base64 representation is equals-free
+    new_length = len(binary_secret) + (3 - len(binary_secret) % 3)
+    return b64encode(binary_secret.rjust(new_length, b" ")).decode()
--- tests/test_secrets.py
+++ tests/test_secrets.py
@@ -1,3 +1,9 @@
+from collections import Counter
+
+import math
+import pytest
+
+
 CUSTOM_SECRET = "0123456789"
 
 
@@ -43,3 +49,31 @@ def test_master_secret_custom_filename(tmp_path, monkeypatch):
     secret_path.write_text("\n" + CUSTOM_SECRET + "\n")
     monkeypatch.setenv("DEREX_MAIN_SECRET_PATH", str(secret_path))
     assert get_master_secret() == CUSTOM_SECRET
+
+    secret_path.write_text("a" * 1024)
+    with pytest.raises(AssertionError):
+        get_master_secret()
+
+
+def test_derived_secret():
+    from derex.runner.secrets import get_secret
+
+    foo_secret = get_secret("foo")
+    # The same name should always yield the same secrets
+    assert get_secret("foo") == foo_secret
+
+    # Two names should have different secrets
+    assert foo_secret != get_secret("bar")
+
+    # Secrets must have enough entropy
+    assert entropy(foo_secret) * len(foo_secret) > 256
+
+    # If we customize the
+
+
+def entropy(s):
+    """Get entropy of string s.
+    Thanks Rosetta code! https://rosettacode.org/wiki/Entropy#Python:_More_succinct_version
+    """
+    p, lns = Counter(s), float(len(s))
+    return -sum(count / lns * math.log(count / lns, 2) for count in p.values())
