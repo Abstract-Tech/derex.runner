0ec5cbd68252fac59da0e023e570bb91ca160682
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-04-28 08:02:14 +0200

    Namespace the xlog MongoDB database

--- derex/runner/settings/derex/mongo.py
+++ derex/runner/settings/derex/mongo.py
@@ -11,4 +11,7 @@ DOC_STORE_CONFIG = {"host": MONGODB_HOST, "db": MONGODB_DB_NAME}
 update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
 
 # This is needed for the Sysadmin dashboard "Git Logs" tab
-MONGODB_LOG = {"host": DOC_STORE_CONFIG["host"], "db": "xlog"}
+MONGODB_LOG = {
+    "host": DOC_STORE_CONFIG["host"],
+    "db": "{}_xlog".format(MONGODB_DB_NAME),
+}
