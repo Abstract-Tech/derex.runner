2ab37315462fe382962615db1b15543a7baed422
Silvio Tomatis <silviot@gmail.com>
2020-05-22 16:16:48 +0200

    Force terminal width to 120 to make rich output predictable

--- tests/test_derex.py
+++ tests/test_derex.py
@@ -122,3 +122,14 @@ def test_derex_cli_group_one_container_running(monkeypatch):
         "These containers are running and exposing an HTTP server on port 80"
         in result.output
     )
+
+
+@pytest.fixture(autouse=True)
+def fix_terminal_width(monkeypatch):
+    from rich.console import Console
+    import derex.runner.cli
+
+    def wrapper(*args, **kwargs):
+        return Console(*args, **dict(kwargs, width=120, force_terminal=True))
+
+    monkeypatch.setattr(derex.runner.cli, "Console", wrapper)
