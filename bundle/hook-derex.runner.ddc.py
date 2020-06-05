bc8f690a8e3fdafbbb70d30d5e83e725a290e4c4
Silvio Tomatis <silviot@gmail.com>
2020-06-05 19:42:29 +0200

    Try to also include scrypt files if it's importable

--- bundle/hook-derex.runner.ddc.py
+++ bundle/hook-derex.runner.ddc.py
@@ -9,3 +9,11 @@ from PyInstaller.utils.hooks import copy_metadata
 datas = copy_metadata("derex.runner")
 datas += collect_data_files("derex.runner")
 datas += collect_data_files("compose", include_py_files=True)
+
+try:
+    import scrypt  # type: ignore # noqa: F403,F401
+
+    datas += copy_metadata("scrypt")
+    datas += collect_data_files("scrypt", include_py_files=True)
+except ImportError:
+    pass
