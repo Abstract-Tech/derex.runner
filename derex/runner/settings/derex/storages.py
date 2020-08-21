541295df2d6af461f8d7f8887dcb19fd4a8ca6ef
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-08-21 19:33:23 +0200

    Fixes to defalt settings for Juniper release
    * Use boto3 if we are on python3
    * Use memcached as CELERY_RESULT_BACKEND if we are on python3

--- derex/runner/settings/derex/storages.py
+++ derex/runner/settings/derex/storages.py
@@ -1,20 +1,25 @@
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+if sys.version_info[0] < 3:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+    AWS_S3_CALLING_FORMAT = "boto.s3.connection.OrdinaryCallingFormat"
+    AWS_S3_HOST = "minio.localhost"
+    AWS_S3_PORT = 80
+    S3_USE_SIGV4 = True
+else:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    AWS_S3_ENDPOINT_URL = "http://minio.localhost"
 
+AWS_S3_CUSTOM_DOMAIN = None
 AWS_ACCESS_KEY_ID = "minio_derex"
 AWS_SECRET_ACCESS_KEY = os.environ.get("DEREX_MINIO_SECRET")
-
-
-AWS_S3_CALLING_FORMAT = "boto.s3.connection.OrdinaryCallingFormat"
-AWS_S3_HOST = "minio.localhost"
-AWS_S3_PORT = 80
 AWS_S3_USE_SSL = False
 AWS_QUERYSTRING_AUTH = True
-S3_USE_SIGV4 = True
+AWS_DEFAULT_ACL = "private"
+AWS_BUCKET_ACL = AWS_DEFAULT_ACL
+
 ORA2_FILEUPLOAD_BACKEND = "s3"
 
 COURSE_IMPORT_EXPORT_STORAGE = DEFAULT_FILE_STORAGE
 USER_TASKS_ARTIFACT_STORAGE = DEFAULT_FILE_STORAGE
-
 
 # Bucket names: they need to be kept in sync with project.py
 FILE_UPLOAD_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME = DEREX_PROJECT
