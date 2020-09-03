fd7e10b5b1ce304b747cc3cd08349764614be73a
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-09-03 09:56:29 +0200

    Fix login CSRF cookie not being set

--- derex/runner/settings/derex/django_settings.py
+++ derex/runner/settings/derex/django_settings.py
@@ -5,6 +5,7 @@ ALLOWED_HOSTS = ["*"]
 # This container should never be exposed directly
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 X_FRAME_OPTIONS = "SAMEORIGIN"
+DCS_SESSION_COOKIE_SAMESITE = None
 
 if "runserver" in sys.argv:
     DEBUG = True
