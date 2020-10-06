c31c65894b174c112ecd89d0106c010dc3a5e80f
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-10-06 10:58:14 +0200

    Fix notes setup.sh script

--- examples/notes/setup.sh
+++ examples/notes/setup.sh
@@ -2,5 +2,5 @@
 
 # This script is temporary, while we figure out how to manage components exactly
 
-ddc-services exec mysql mysql -psecret -e "CREATE DATABASE IF NOT EXISTS derex_notes"
+ddc-services exec mysql mysql -p$(derex debug print-secret mysql) -e "CREATE DATABASE IF NOT EXISTS derex_notes"
 ddc-project run --rm notes ./manage.py migrate
