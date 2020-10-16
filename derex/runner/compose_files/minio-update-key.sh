282dc051e77e4962932880e2ac6031b4bbddd2e3
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2020-10-16 02:01:43 +0200

    Rewritten update minio key procedure

---
+++ derex/runner/compose_files/minio-update-key.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# We need to stop minio after it's done re-keying. To this end, we use the expect package
+apk add expect --no-cache
+
+#We need to make sure the current credentials are not working...
+RESULT="$(expect -c 'spawn /usr/bin/minio server /data; expect "SecretKey:";')"
+if [[ $(echo $RESULT | grep -c $MINIO_SECRET_KEY) -gt 0 ]]; then
+    echo "Credentials already updated, nothing do here!"
+    exit 1
+fi
+
+# ..but the old ones are
+RESULT="$(MINIO_SECRET_KEY=$MINIO_SECRET_KEY_OLD expect -c 'spawn /usr/bin/minio server /data; expect "SecretKey:";')"
+if [[ $(echo $RESULT | grep -c $MINIO_SECRET_KEY_OLD) -eq 0 ]]; then
+    echo "Invalid old credentials!"
+    exit 1
+fi
+
+export MINIO_ACCESS_KEY_OLD="$MINIO_ACCESS_KEY" MINIO_SECRET_KEY_OLD="$MINIO_SECRET_KEY_OLD"
+RESULT="$(expect -c 'spawn /usr/bin/minio server /data; expect "SecretKey:";')"
+if [[ $(echo $RESULT | grep -c "$MINIO_SECRET_KEY") -eq 0 ]]; then
+    echo "Unexpected error occurred during MinIO secret key rotation"
+    expect -c 'spawn /usr/bin/minio server /data; { close; exit 1} "'
+    exit 1
+fi
+
+echo "Minio server key rotation completed"
+exit 0
--- derex/runner/compose_generation.py
+++ derex/runner/compose_generation.py
@@ -8,17 +8,22 @@ The functions have to be reachable under the common name `ddc_project_options`
 so a class is put in place to hold each of them.
 """
 from derex.runner import hookimpl
+from derex.runner.constants import DDC_ADMIN_PATH
+from derex.runner.constants import DDC_PROJECT_TEMPLATE_PATH
+from derex.runner.constants import DDC_SERVICES_YML_PATH
+from derex.runner.constants import DEREX_DJANGO_PATH
+from derex.runner.constants import DEREX_ETC_PATH
+from derex.runner.constants import DEREX_OPENEDX_CUSTOMIZATIONS_PATH
 from derex.runner.constants import MONGODB_ROOT_USER
+from derex.runner.constants import WSGI_PY_PATH
 from derex.runner.docker_utils import image_exists
 from derex.runner.local_appdir import DEREX_DIR
 from derex.runner.local_appdir import ensure_dir
 from derex.runner.project import Project
 from derex.runner.secrets import DerexSecrets
 from derex.runner.secrets import get_secret
-from derex.runner.utils import abspath_from_egg
 from derex.runner.utils import asbool
 from distutils import dir_util
-from functools import partial
 from jinja2 import Template
 from pathlib import Path
 from typing import Dict
@@ -31,6 +36,7 @@ import os
 
 logger = logging.getLogger(__name__)
 
+<<<<<<< HEAD
 DEREX_ETC_PATH = Path("/etc/derex")
 derex_path = partial(abspath_from_egg, "derex.runner")
 
@@ -49,6 +55,8 @@ assert all(
     (WSGI_PY_PATH, DDC_SERVICES_YML_PATH, DDC_ADMIN_PATH, DDC_PROJECT_TEMPLATE_PATH,)
 ), "Some distribution files were not found"
 
+=======
+>>>>>>> Rewritten update minio key procedure
 
 class BaseServices:
     @staticmethod
@@ -146,20 +154,10 @@ def generate_ddc_project_file(project: Project) -> Path:
             f"Image {project.requirements_image_name} not found\n"
             "Run\nderex build requirements\n to build it"
         )
-    derex_django_path = abspath_from_egg(
-        "derex.runner", "derex_django/README.rst"
-    ).parent
 
-    derex_openedx_customizations_dir = (
-        abspath_from_egg(
-            "derex.runner",
-            "derex/runner/compose_files/openedx_customizations/README.rst",
-        ).parent
-        / project.openedx_version.name
-    )
     openedx_customizations = []
     for openedx_customizations_dir in [
-        derex_openedx_customizations_dir,
+        DEREX_OPENEDX_CUSTOMIZATIONS_PATH / project.openedx_version.name,
         project.openedx_customizations_dir,
     ]:
         if openedx_customizations_dir and openedx_customizations_dir.exists():
@@ -178,7 +176,7 @@ def generate_ddc_project_file(project: Project) -> Path:
         project=project,
         final_image=final_image,
         wsgi_py_path=WSGI_PY_PATH,
-        derex_django_path=derex_django_path,
+        derex_django_path=DEREX_DJANGO_PATH,
         openedx_customizations=openedx_customizations,
     )
     project_compose_path.write_text(text)
