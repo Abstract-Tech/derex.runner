d80d7dbba986c3977c163a86ff36e54e236e8d99
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2021-01-26 18:25:54 +0100

    Use the same constraints file when building the openedx image and the project requirements image

--- derex/runner/build.py
+++ derex/runner/build.py
@@ -12,16 +12,12 @@ logger = logging.getLogger(__name__)
 def docker_commands_to_install_requirements(project: Project):
     dockerfile_contents = []
     if project.requirements_dir:
-        dockerfile_contents.append(
-            # Constrain edx version, but omit the relative paths: we run this from our
-            # requirements dir so that the derex user can use `./` in their requirements files
-            "RUN grep == /openedx/edx-platform/requirements/edx/development.txt |grep -v ^git+https > /tmp/development.txt\n"
-            "COPY requirements /openedx/derex.requirements/\n"
-        )
+        dockerfile_contents.append("COPY requirements /openedx/derex.requirements/\n")
         for requirments_file in os.listdir(project.requirements_dir):
             if requirments_file.endswith(".txt"):
                 dockerfile_contents.append(
-                    f"RUN cd /openedx/derex.requirements && pip install -c /tmp/development.txt -r {requirments_file}\n"
+                    "RUN cd /openedx/derex.requirements && "
+                    f"pip install -r {requirments_file} -c /openedx/requirements/openedx_constraints.txt\n"
                 )
     return dockerfile_contents
 
