780a6eb5e3f169e51855214a1f7d822dba7e09cd
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2021-02-10 19:15:32 +0100

    Fix grades and financial report storages

--- derex/runner/settings/derex/storages.py
+++ derex/runner/settings/derex/storages.py
@@ -27,15 +27,16 @@ FILE_UPLOAD_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME = DEREX_PROJECT
 FILE_UPLOAD_STORAGE_PREFIX = "submissions_attachments"
 
 GRADES_DOWNLOAD = {
-    "BUCKET": AWS_STORAGE_BUCKET_NAME,
-    "ROOT_PATH": "grades",
-    "STORAGE_TYPE": "s3",
+    "STORAGE_CLASS": DEFAULT_FILE_STORAGE,
+    "STORAGE_KWARGS": {"bucket": AWS_STORAGE_BUCKET_NAME, "location": "grades",},
 }
 
 FINANCIAL_REPORTS = {
-    "BUCKET": AWS_STORAGE_BUCKET_NAME,
-    "ROOT_PATH": "financial-reports",
-    "STORAGE_TYPE": "s3",
+    "STORAGE_CLASS": DEFAULT_FILE_STORAGE,
+    "STORAGE_KWARGS": {
+        "bucket": AWS_STORAGE_BUCKET_NAME,
+        "location": "financial-reports",
+    },
 }
 
 # This is needed for the Sysadmin dashboard "Git Logs" tab
