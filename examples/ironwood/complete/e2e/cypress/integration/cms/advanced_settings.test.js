cd13f8194307a3ac7edee8cf11133d302afcfb13
Laith massoud <laythmassoud8@gmail.com>
2021-03-17 19:41:28 +0100

    add some cms tests

--- examples/ironwood/complete/e2e/cypress/integration/cms/advanced_settings.test.js
+++ examples/ironwood/complete/e2e/cypress/integration/cms/advanced_settings.test.js
@@ -1,39 +1,32 @@
 describe("advanced settings test", () => {
-  const lms_url = Cypress.env("LMS_URL");
-  const course_id = Cypress.env("DEMO_COURSE_ID");
-  const cms_url = Cypress.env("CMS_URL");
-  const demo_course = `${cms_url}/course/${course_id}`;
-
-  it("allows admin to change sittings ", () => {
-
-    cy.visit(lms_url)
-    cy.login(Cypress.env("user_email"), Cypress.env("user_password"));
-    cy.visit(demo_course)
-
-    // go to setting then advanced settings
-
-
-    cy
-      .get(".nav-course-settings > .title")
-      .wait(1500)
-      .click({ force: true });
-
-    cy
-    cy.get('.nav-course-settings-advanced > a')
-
-      .click({ force: true });
-    /* check the input of Allow Anonymous Discussion Posts to Peer if the contains is false */
-
-    cy
-      .get(
-        ":nth-child(3) > .value > .CodeMirror > .CodeMirror-scroll > .CodeMirror-sizer  .CodeMirror-lines  pre .cm-atom"
-      )
-      .contains("false");
-    /* check the input of Course Visibility In Catalog if the contains is both */
-    cy
-      .get(
-        ":nth-child(24) > .value > .CodeMirror > .CodeMirror-scroll > .CodeMirror-sizer .CodeMirror-lines pre .cm-string"
-      )
-      .contains("both");
+    const cms_url = Cypress.env("CMS_URL");
+    const course_id = Cypress.env("DEMO_COURSE_ID");
+    const demo_course = `${cms_url}/course/${course_id}`;
+  
+    it("allows admin to change sittings ", () => {
+      cy.login_CMS(Cypress.env("edx_user"), Cypress.env("edx_password"));
+  
+      // visit the demo course
+      cy.visit(demo_course);
+  
+      // go to setting then advanced settings
+      cy
+        .get(".nav-course-settings-advanced > a")
+        .wait(1500)
+        .click({ force: true });
+  
+      /* check the input of Allow Anonymous Discussion Posts to Peer if the contains is false */
+  
+      cy
+        .get(
+          ":nth-child(3) > .value > .CodeMirror > .CodeMirror-scroll > .CodeMirror-sizer  .CodeMirror-lines  pre .cm-atom"
+        )
+        .contains("false");
+      /* check the input of Course Visibility In Catalog if the contains is both */
+      cy
+        .get(
+          ":nth-child(24) > .value > .CodeMirror > .CodeMirror-scroll > .CodeMirror-sizer .CodeMirror-lines pre .cm-string"
+        )
+        .contains("both");
+    });
   });
-});
