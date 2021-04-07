ce91dfbb35117dc47b97827f7563fdc16934289b
Laith massoud <laythmassoud8@gmail.com>
2021-04-07 17:30:14 +0200

    add cms tests

--- examples/ironwood/complete/e2e/cypress/integration/cms/exportCourses.test.js
+++ examples/ironwood/complete/e2e/cypress/integration/cms/exportCourses.test.js
@@ -3,11 +3,10 @@ describe("Studio home page", () => {
   const lms_url = Cypress.env("LMS_URL");
   const course_id = Cypress.env("DEMO_COURSE_ID");
   const export_url = `${cms_url}/export/${course_id}`;
-  console.log(export_url);
   it("test if an Admin able import course", async () => {
     cy.visit(lms_url);
     cy.login(Cypress.env("user_email"), Cypress.env("user_password"));
     cy.visit(export_url);
-    cy.get("a.action-export").click();
+    cy.get("a.action-export").click({force:"true"});
   });
 });
