483157bf75ba1bfed312a59c887f942688db045a
Laith massoud <laythmassoud8@gmail.com>
2021-04-15 16:36:05 +0200

    fix the pipline Juniper

--- examples/juniper/complete/e2e/cypress/integration/lms/courseSearch.test.js
+++ examples/juniper/complete/e2e/cypress/integration/lms/courseSearch.test.js
@@ -1,5 +1,6 @@
 describe("Courses Page", () => {
   const lms_url = Cypress.env("LMS_URL");
+  const cms_url = Cypress.env("CMS_URL");
 
   // previous test omitted for brevity
   Cypress.on("uncaught:exception", (err, runnable) => {
@@ -10,33 +11,24 @@ describe("Courses Page", () => {
 
   it("test after you logged in you are able to navigate to courses page and check if there is courses or not  ", function () {
     cy.login(Cypress.env("user_email"), Cypress.env("user_password"));
-    cy.visit("/dashboard");
-
-    cy.get("body")
-      .find("#my-courses")
+    cy.visit(cms_url);
+    cy.createCourse();
+    cy.wait(2000);
+    cy.visit(`${lms_url}/courses`);
+    cy.get(".course").should("be.visible");
+    cy.get("li.course-item:first-child")
+      .find("h3.course-title")
       .then((res) => {
-        if (res.find(".empty-dashboard-message").length > 0) {
-          console.log("No Courses");
-        } else {
-          cy.get(".course").should("be.visible");
-          cy.get("li.course-item:first-child")
-            .find("h3.course-title")
-            .then((res) => {
-              cy.get("#dashboard-search-input").type(res[0].outerText);
-              cy.get(".search-button").click();
-              cy.get("body")
-                .find("#dashboard-search-results")
-                .then((res) => {
-                  if (
-                    cy.get(res).children("ol.search-result-list").length > 0
-                  ) {
-                    cy.get("li.search-results-item").should("be.visible");
-                  } else {
-                    console.log("No courses founded");
-                  }
-                });
-            });
-        }
+        cy.get("#dashboard-search-input").type(res[0].outerText);
+        cy.get(".search-button").click();
+
+        cy.get("body")
+          .find("#dashboard-search-results")
+          .then((res) => {
+            if (cy.get(res).children("ol.search-result-list").length > 0) {
+              cy.get("li.search-results-item").should("be.visible");
+            }
+          });
       });
   });
 });
