4bfd1782650f2fd80c417d61fd48dcb5b51cc384
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2021-05-25 09:44:08 +0200

    Bump version: 0.2.2.dev1 → 0.3.0

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extras_requirements,
     url="https://github.com/Abstract-Tech/derex.runner",
-    version="0.2.2.dev1",
+    version="0.3.0",
     zip_safe=False,
 )
