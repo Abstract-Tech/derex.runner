ff2674d7981be7910ec9254516de8a38a7104901
serhii <sereja.kostenko@gmail.com>
2022-04-07 22:57:22 +0200

    Fix duplicate alias and pin click==8.0.4 to fix black

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("HISTORY.rst") as history_file:
 
 requirements = [
     "appdirs",
-    "Click==8.1.0",
+    "Click==8.0.4",
     "click_plugins",
     "docker-compose>=1.25.4",
     "importlib_metadata",
