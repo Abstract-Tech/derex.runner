690150d41d4c0a4616c7de1af6003f26382b7154
serhii <sereja.kostenko@gmail.com>
2022-04-13 18:58:44 +0200

    Fix linting issues

--- examples/juniper/complete/settings/base.py
+++ examples/juniper/complete/settings/base.py
@@ -4,7 +4,6 @@ from derex_django.settings.default import *  # noqa: F401, F403
 # Id of the site fixture to use, instead of looking up the hostname
 SITE_ID = 1
 
-
 FOOTER_OPENEDX_LOGO_IMAGE = (
     "https://files.edx.org/openedx-logos/edx-openedx-logo-tag.png"
 )
