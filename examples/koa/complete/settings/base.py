4a5202669bf6137fcbc076895d715e9b89dd52d3
serhii <sereja.kostenko@gmail.com>
2022-04-13 17:06:21 +0200

    Changed logo path

--- examples/koa/complete/settings/base.py
+++ examples/koa/complete/settings/base.py
@@ -3,5 +3,5 @@ from derex_django.settings.default import *  # noqa: F401, F403
 
 # Id of the site fixture to use, instead of looking up the hostname
 SITE_ID = 1
-LOGO_URL = "{}/static/demo-theme/lms/static/images/logo.png".format(LMS_ROOT_URL)  # noqa: F405
+LOGO_URL = "{}/static/demo-theme/images/logo.png".format(LMS_ROOT_URL)  # noqa: F405
 LOGO_URL_PNG = LOGO_URL
